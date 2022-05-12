a01b90450c826ad6646f978a35f2de96db13509a
Luiz Alcides Zytkowski Junior <luiz.alcides@softniels.com.br>
2022-05-12 10:28:45 -0300

    Server.kt: Removed comments + added custom port to Javalin creation

--- server/src/Server.kt
+++ server/src/Server.kt
@@ -7,23 +7,19 @@ import org.ktorm.database.Database
 class Server {
 
     fun start() {
-        // 1. Read config
         val lConfig = ConfigLoaderBuilder.default()
             .addResourceSource("/applicationConfig.json")
             .build()
             .loadConfigOrThrow<ServerConfigurationDTO>()
-        // 2. Start DI
         val lDatabase: Database = DatabaseProvider.getDatabaseConnectionAndMigrate(
             aDatabaseConfigurationDTO = lConfig.database
         )
-
         DependencyContainer.bootstrap(
             aDatabase = lDatabase
         )
-        // 3. Migrate DB
         Javalin.create {
 
-        }.start(7070)
+        }.start(lConfig.port)
     }
 
 }
