62d557148da1234e53758eff7906a57253215aff
Luiz Alcides Zytkowski Junior <luiz.alcides@softniels.com.br>
2022-05-12 11:45:43 -0300

    Refactor de DatabaseProvider.kt para múltiplas funções internas

--- shared/src/DatabaseProvider.kt
+++ shared/src/DatabaseProvider.kt
@@ -5,23 +5,37 @@ import framework.exception.DatabaseConfigurationException
 import org.flywaydb.core.Flyway
 import org.ktorm.database.Database
 import org.ktorm.support.postgresql.PostgreSqlDialect
+import javax.sql.DataSource
 
 object DatabaseProvider {
 
-    private enum class DatabaseProviders {
+    private enum class DatabaseProvider {
         PGSQL,
         H2
     }
 
-    fun getDatabaseConnectionAndMigrate(aDatabaseConfigurationDTO: DatabaseConfigurationDTO): Database {
-        val lDatabaseProvider = try {
-            DatabaseProviders.valueOf(aDatabaseConfigurationDTO.provider)
-        } catch (e: Exception) {
-            throw DatabaseConfigurationException("Invalid database provider: '${aDatabaseConfigurationDTO.provider}'. Use one of: ${DatabaseProviders.values()}")
+    private fun migrate(aDatabaseProvider: DatabaseProvider, aDataSource: DataSource) {
+        when (aDatabaseProvider) {
+            DatabaseProvider.PGSQL -> {
+                Flyway.configure()
+                    .dataSource(aDataSource)
+                    .locations("migrations/pgsql")
+                    .load()
+                    .migrate()
+            }
+            DatabaseProvider.H2 -> {
+                Flyway.configure()
+                    .dataSource(aDataSource)
+                    .locations("migrations/h2")
+                    .load()
+                    .migrate()
+            }
         }
+    }
 
-        val lDataSource = when (lDatabaseProvider) {
-            DatabaseProviders.PGSQL -> {
+    private fun getDataSource(aDatabaseProvider: DatabaseProvider, aDatabaseConfigurationDTO: DatabaseConfigurationDTO) : DataSource {
+        return when (aDatabaseProvider) {
+            DatabaseProvider.PGSQL -> {
                 HikariDataSource(
                     HikariConfig().apply {
                         driverClassName = "org.postgresql.Driver"
@@ -36,7 +50,7 @@ object DatabaseProvider {
                     }
                 )
             }
-            DatabaseProviders.H2 -> {
+            DatabaseProvider.H2 -> {
                 HikariDataSource(
                     HikariConfig().apply {
                         driverClassName = "org.h2.Driver"
@@ -50,16 +64,19 @@ object DatabaseProvider {
                 )
             }
         }
+    }
 
-        Flyway.configure()
-            .dataSource(lDataSource)
-            .locations("migrations")
-            .load()
-            .migrate()
-
+    fun getDatabaseConnectionAndMigrate(aDatabaseConfigurationDTO: DatabaseConfigurationDTO): Database {
+        val lDatabaseProvider = try {
+            DatabaseProvider.valueOf(aDatabaseConfigurationDTO.provider)
+        } catch (e: Exception) {
+            throw DatabaseConfigurationException("Invalid database provider: '${aDatabaseConfigurationDTO.provider}'. Use one of: ${DatabaseProvider.values()}")
+        }
+        val lDataSource = getDataSource(lDatabaseProvider, aDatabaseConfigurationDTO)
+        migrate(lDatabaseProvider, lDataSource)
         return when (lDatabaseProvider) {
-            DatabaseProviders.PGSQL -> Database.connect(dataSource = HikariDataSource(lDataSource), dialect = PostgreSqlDialect())
-            DatabaseProviders.H2 -> Database.connect(dataSource = HikariDataSource(lDataSource))
+            DatabaseProvider.PGSQL -> Database.connect(dataSource = lDataSource, dialect = PostgreSqlDialect())
+            DatabaseProvider.H2 -> Database.connect(dataSource = lDataSource)
         }
     }
 
