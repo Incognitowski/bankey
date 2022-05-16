3dbe727747d8114533ed6255c8aced6238f8616c
Luiz Alcides Zytkowski Junior <luiz.alcides@softniels.com.br>
2022-05-16 15:00:10 -0300

    ParameterAPI.kt: Complementada lógica do update

--- shared/src/parameter/ParameterAPI.kt
+++ shared/src/parameter/ParameterAPI.kt
@@ -1,10 +1,16 @@
 package parameter
 
+import framework.exception.EntityNotFoundException
+
 class ParameterAPI(private val mParameterRepository: ParameterRepository) {
 
     fun create(aParameterEntity: ParameterEntity) = mParameterRepository.create(aParameterEntity)
 
-    fun update(aParameterEntity: ParameterEntity) = mParameterRepository.update(aParameterEntity)
+    fun update(aParameterEntity: ParameterEntity) {
+        mParameterRepository.findById(aParameterEntity.parameterId)
+            ?: throw EntityNotFoundException("Parameter with ID ${aParameterEntity.parameterId} not found.")
+        mParameterRepository.update(aParameterEntity)
+    }
 
     fun findLatest(): ParameterEntity? = mParameterRepository.findLatest()
 
