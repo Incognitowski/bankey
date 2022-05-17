8143c11d235723f2d08ee2dc79d1b8558a821bcc
Junior <junior.zytkowski@gmail.com>
2022-05-16 23:07:25 -0300

    DependencyContainer.kt refactores to new invokation style

--- shared/src/DependencyContainer.kt
+++ shared/src/DependencyContainer.kt
@@ -1,15 +1,17 @@
-import framework.databaseDependencyModule
+import framework.DatabaseDependencyModule
+import operation.OperationDependencyModule
 import org.koin.core.context.startKoin
 import org.ktorm.database.Database
-import parameter.parameterDependencyModule
+import parameter.ParameterDependencyModule
 
 object DependencyContainer {
 
     fun bootstrap(aDatabase: Database) {
         startKoin {
             modules(
-                databaseDependencyModule(aDatabase),
-                parameterDependencyModule()
+                DatabaseDependencyModule(aDatabase),
+                ParameterDependencyModule(),
+                OperationDependencyModule(),
             )
         }
     }
