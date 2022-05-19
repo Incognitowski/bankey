046566dbe041507d5a5f19c79517b4fd016c2c6c
Luiz Alcides Zytkowski Junior <luiz.alcides@softniels.com.br>
2022-05-19 17:07:17 -0300

    AnyExtensionFunctions.kt: registered JavaTimeModule() on jacksonObjectMapper

--- shared/src/framework/extensionFunctions/AnyExtensionFunctions.kt
+++ shared/src/framework/extensionFunctions/AnyExtensionFunctions.kt
@@ -1,7 +1,11 @@
 package framework.extensionFunctions
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 fun Any.toJSON() : String {
-    return jacksonObjectMapper().writeValueAsString(this)
+    val lObjectMapper = jacksonObjectMapper().also {
+        it.registerModule(JavaTimeModule())
+    }
+    return lObjectMapper.writeValueAsString(this)
 }
