455df1af0ef72f542115309bcd737bdf2384a558
Junior <junior.zytkowski@gmail.com>
2022-05-18 21:14:46 -0300

    Added OperationEventDependencyModule invocation on DependencyContainer.kt

--- shared/src/DependencyContainer.kt
+++ shared/src/DependencyContainer.kt
@@ -1,5 +1,6 @@
 import framework.DatabaseDependencyModule
 import operation.OperationDependencyModule
+import operationEvent.OperationEventDependencyModule
 import org.koin.core.context.startKoin
 import org.ktorm.database.Database
 import parameter.ParameterDependencyModule
@@ -11,6 +12,7 @@ object DependencyContainer {
             modules(
                 DatabaseDependencyModule(aDatabase),
                 ParameterDependencyModule(),
+                OperationEventDependencyModule(),
                 OperationDependencyModule(),
             )
         }
