6de6d334e7a5a237dbd02f68ad1f0221022d3b17
Luiz Alcides Zytkowski Junior <luiz.alcides@softniels.com.br>
2022-05-20 14:44:48 -0300

    build.gradle.kts: Added javalin test tools

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -7,6 +7,10 @@ dependencies {
     implementation(project(":shared"))
 
     implementation("io.javalin:javalin-bundle:4.6.0")
+
+    testImplementation("io.javalin:javalin-testtools:4.6.0")
+    testImplementation("org.assertj:assertj-core:3.22.0")
+
 }
 
 kotlin.sourceSets["main"].kotlin.srcDirs("src")
